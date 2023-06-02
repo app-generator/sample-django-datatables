416738ba86be7bd9e121887ea809346718cfb01e
BenFaruna <benjaminfaruna@gmail.com>
2023-06-02 13:11:15 +0100

    Fixed error created by using new version of django

---
+++ app/middlewares.py
@@ -0,0 +1,11 @@
+class AjaxMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        def is_ajax(self):
+            return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+        
+        request.is_ajax = is_ajax.__get__(request)
+        response = self.get_response(request)
+        return response
--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.middlewares.AjaxMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
